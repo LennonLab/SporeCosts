a8198162954f1f4ba3666c4f75f718fbf1af3829
William R. Shoemaker <williamrshoemaker@gmail.com>
2024-04-26 01:58:46 +0200

    init model and pop gen analyses

---
+++ scripts/config.py
@@ -0,0 +1,13 @@
+###############################################################################
+#
+# Set up default source and output directories
+#
+###############################################################################
+import os.path
+import os
+from math import log10
+
+
+data_directory = os.path.expanduser("~/GitHub/SporeCosts/data/")
+analysis_directory = os.path.expanduser("~/GitHub/SporeCosts/figures/")
+scripts_directory = os.path.expanduser("~/GitHub/SporeCosts/scripts/")
